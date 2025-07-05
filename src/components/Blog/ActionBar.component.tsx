8b470e7c2cfecfaa4126e8bf0b8c0120a8b884f7
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-05 20:16:02 +0330

    feat: Add reFetch functionality

--- src/components/Blog/ActionBar.component.tsx
+++ src/components/Blog/ActionBar.component.tsx
@@ -1,29 +1,38 @@
-import { AiFillPlusCircle } from "react-icons/ai";
+import { AiFillInteraction, AiFillPlusCircle } from "react-icons/ai";
 
 interface Props {
   searchedValue: string;
   onSearchedValueChange: (value: string) => void;
+  itemCount: number;
+  onRefresh: () => void;
 }
 
-const ActionBar = ({ searchedValue, onSearchedValueChange }: Props) => {
+const ActionBar = ({ searchedValue, onSearchedValueChange, itemCount, onRefresh }: Props) => {
   return (
     <Card className="fixed bottom-0 left-[50%] z-10 flex w-full translate-x-[-50%] flex-col gap-2 rounded-none border-0 px-8 py-4 opacity-95 hover:border-0 md:bottom-4 md:w-124 md:rounded-xl">
       <div className="flex items-center justify-between">
         <ThemeToggleButton />
 
-        <Modal.Open
-          render={({ open }) => (
-            <BaseButton icon onClick={open}>
-              <AiFillPlusCircle />
-            </BaseButton>
-          )}
-        />
+        <div className="flex gap-4">
+          <BaseButton icon color="emerald" onClick={onRefresh}>
+            <AiFillInteraction />
+          </BaseButton>
+
+          <Modal.Open
+            render={({ open }) => (
+              <BaseButton icon onClick={open}>
+                <AiFillPlusCircle />
+              </BaseButton>
+            )}
+          />
+        </div>
       </div>
 
       <TextField
         label="جستجو"
         name="search"
         value={searchedValue}
+        disabled={!itemCount}
         onChange={(event) => onSearchedValueChange(event.target.value)}
       />
     </Card>
--- src/views/Blog/Root.view.tsx
+++ src/views/Blog/Root.view.tsx
@@ -3,7 +3,11 @@ import service from "@/service/Blog";
 import { ActionBar, PostList } from "@/components/Blog";
 
 const Root = () => {
-  const { data: posts, loading } = usePromise(service.getAll, {
+  const {
+    data: posts,
+    loading,
+    reExecute,
+  } = usePromise(service.getAll, {
     defaultData: [],
     immediate: true,
     ttl: 3600 * 1000,
@@ -14,9 +18,20 @@ const Root = () => {
   return (
     <div className="px-6 py-8">
       <Modal>
-        <ActionBar searchedValue={searchedValue} onSearchedValueChange={setSearchedValue} />
+        <ActionBar
+          searchedValue={searchedValue}
+          itemCount={posts.length}
+          onSearchedValueChange={setSearchedValue}
+          onRefresh={reExecute}
+        />
 
-        <div className="size-full pb-32">{loading ? <Loading /> : <PostList items={searchedPosts} />}</div>
+        <div className="size-full pb-32">
+          {loading ? (
+            <FullPageLoading message="در حال بارگیری پست ها..." />
+          ) : (
+            <PostList items={searchedPosts} />
+          )}
+        </div>
 
         <Modal.Window title="افزودن" render={() => <div>modal</div>} />
       </Modal>
