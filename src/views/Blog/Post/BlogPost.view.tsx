3b1fa69ebab03a6f9a43f8d517c805e3b57a44f8
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-12 18:20:44 +0330

    fix: Fix post style when overflowing

--- src/views/Blog/Post/BlogPost.view.tsx
+++ src/views/Blog/Post/BlogPost.view.tsx
@@ -2,11 +2,11 @@ import { AboutUsActionBar, PostDetails } from "@/components/Blog";
 
 const BlogPost = () => {
   return (
-    <>
+    <div className="pb-16 sm:pb-24">
       <PostDetails />
 
       <AboutUsActionBar />
-    </>
+    </div>
   );
 };
 
