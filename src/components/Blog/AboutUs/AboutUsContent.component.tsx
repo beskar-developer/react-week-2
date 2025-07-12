9154106d98b8feeee5d14966799b9a6b705e0a32
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-12 16:23:41 +0330

    chore: Add animation to about us page

--- src/components/Blog/AboutUs/AboutUsContent.component.tsx
+++ src/components/Blog/AboutUs/AboutUsContent.component.tsx
@@ -1,13 +1,15 @@
+import { itemAnimationConfig } from "@shared-vendor/constants";
+
 import AboutUsHeader from "./AboutUsHeader.component";
 import AboutUsWriterList from "./AboutUsWriterList.component";
 
 const AboutUsContent = () => {
   return (
-    <>
+    <motion.div className="flex flex-col gap-6 px-4 py-6" {...itemAnimationConfig}>
       <AboutUsHeader />
 
       <AboutUsWriterList />
-    </>
+    </motion.div>
   );
 };
 
--- src/views/Blog/AboutUs/BlogAboutUs.view.tsx
+++ src/views/Blog/AboutUs/BlogAboutUs.view.tsx
@@ -3,7 +3,7 @@ import { AboutUsActionBar, AboutUsContent } from "@/components/Blog";
 const BlogAboutUs = () => {
   return (
     <div className="pb-16 sm:pb-24">
-      <Card className="flex size-full flex-col gap-6 px-4 py-6">
+      <Card className="size-full">
         <AboutUsContent />
       </Card>
 
